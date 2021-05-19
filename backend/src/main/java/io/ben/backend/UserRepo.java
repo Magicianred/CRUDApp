db6f4b57f7d5ccedffa21b1f036b2a052fac558d
benmai22 <bcmai@cpp.edu>
2021-05-19 14:23:08 -0700

    First commit

---
+++ backend/src/main/java/io/ben/backend/UserRepo.java
@@ -0,0 +1,8 @@
+package io.ben.backend;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepo extends JpaRepository<User,Integer> {
+
+}
